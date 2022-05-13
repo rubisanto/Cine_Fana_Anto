a79c83ca616016e309db489da045bcdbd5c88deb
rubisanto <antoinegogceapb@hotmail.fr>
2022-05-13 14:21:19 +0200

    Mise à jour d'accès à la Database sur MAMP

--- models/Database.php
+++ models/Database.php
@@ -1,10 +1,12 @@
 <?php
 
 // Class qui permet de se connecter a la bdd
-class Database {
-  protected function connectionDatabase() {
+class Database
+{
+  protected function connectionDatabase()
+  {
     try {
-      $database = new PDO("mysql:host=localhost;dbname=cine_fana;charset=utf8", "root", "");
+      $database = new PDO("mysql:host=localhost;dbname=cine_fana;charset=utf8", "root", "root");
     } catch (\Exception $e) {
       echo "Erreur: " . $e->getMessage();
     }
